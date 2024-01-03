70add8f938611af3b17ec044f71dd44a895fb9f6
Arash Heidari <arash.heidari@ugent.be>
2024-01-03 14:11:13 +0100

    test completed

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import matplotlib.pyplot  as plt
+import numpy as np
 
-from solver   import solver
-from config   import Setting
-
-
+from solver import solver
+from config import Setting
 
 rhvs = []
 rd2l = []
-while(len(rhvs) < 1):
+while(len(rhvs) < Setting.repetitions):
     try:
         print(len(rhvs))
         regret_d2l, regret_hv, model, dataset = solver(Setting.problem, Setting.method, reps = 80)
         rhvs.append(regret_hv)
         rd2l.append(regret_d2l)
-    except KeyboardInterrupt:
-        break
+    except:
+        pass
+
+np.save(f"./HV Regret of {Setting.problem} with {Setting.knees} knee(s) and input dimension of {Setting.n_var}", rhvs)
+np.save(f"./Distance Regret of {Setting.problem} with {Setting.knees} knee(s) and input dimension of {Setting.n_var}", rd2l)
